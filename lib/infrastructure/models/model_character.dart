abbc2860fdbf9185d615587e3e2071b4945faac1
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-31 15:31:30 -0500

    Class ModelCharacter creation

---
+++ lib/infrastructure/models/model_character.dart
@@ -0,0 +1,119 @@
+import 'dart:convert';
+import 'package:rick_and_morty/infrastructure/models/model_info.dart';
+
+
+ModelCharacter modelCharacterFromJson(String str) =>
+    ModelCharacter.fromJson(json.decode(str));
+
+String modelCharacterToJson(ModelCharacter data) => json.encode(data.toJson());
+
+class ModelCharacter {
+  final Info? info;
+  final List<Result>? results;
+
+  ModelCharacter({
+    this.info,
+    this.results,
+  });
+
+  factory ModelCharacter.fromJson(Map<String, dynamic> json) => ModelCharacter(
+        info: json["info"] == null ? null : Info.fromJson(json["info"]),
+        results: json["results"] == null
+            ? []
+            : List<Result>.from(
+                json["results"]!.map((x) => Result.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "info": info?.toJson(),
+        "results": results == null
+            ? []
+            : List<dynamic>.from(results!.map((x) => x.toJson())),
+      };
+}
+
+class Result {
+  final int? id;
+  final String? name;
+  final String? status;
+  final String? species;
+  final String? type;
+  final String? gender;
+  final Location? origin;
+  final Location? location;
+  final String? image;
+  final List<String>? episode;
+  final String? url;
+  final String? created;
+
+  Result({
+    this.id,
+    this.name,
+    this.status,
+    this.species,
+    this.type,
+    this.gender,
+    this.origin,
+    this.location,
+    this.image,
+    this.episode,
+    this.url,
+    this.created,
+  });
+
+  factory Result.fromJson(Map<String, dynamic> json) => Result(
+        id: json["id"],
+        name: json["name"],
+        status: json["status"],
+        species: json["species"],
+        type: json["type"],
+        gender: json["gender"],
+        origin:
+            json["origin"] == null ? null : Location.fromJson(json["origin"]),
+        location: json["location"] == null
+            ? null
+            : Location.fromJson(json["location"]),
+        image: json["image"],
+        episode: json["episode"] == null
+            ? []
+            : List<String>.from(json["episode"]!.map((x) => x)),
+        url: json["url"],
+        created: json["created"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "status": status,
+        "species": species,
+        "type": type,
+        "gender": gender,
+        "origin": origin?.toJson(),
+        "location": location?.toJson(),
+        "image": image,
+        "episode":
+            episode == null ? [] : List<dynamic>.from(episode!.map((x) => x)),
+        "url": url,
+        "created": created,
+      };
+}
+
+class Location {
+  final String? name;
+  final String? url;
+
+  Location({
+    this.name,
+    this.url,
+  });
+
+  factory Location.fromJson(Map<String, dynamic> json) => Location(
+        name: json["name"],
+        url: json["url"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "url": url,
+      };
+}
