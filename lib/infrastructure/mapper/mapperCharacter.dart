1c42b531af2eb8c92f34da084bf072a54040232c
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-31 18:43:14 -0500

    Class EnumValues creation and its instatances genderValues and statusValues

--- lib/infrastructure/mapper/mapperCharacter.dart
+++ lib/infrastructure/mapper/mapperCharacter.dart
@@ -6,23 +6,53 @@ class MapperCharacter {
   static Character characterModleToCharacterEntity( ResultCharacter resultCharacter ) {
     return Character(
      id: resultCharacter.id ?? 999333,
-     name: resultCharacter.name ?? "unknow",
-     status: resultCharacter.status ?? "unknow",
-     species: resultCharacter.species ?? "unknow",
-     type: resultCharacter.type ?? "unknow",
-     gender: resultCharacter.gender ?? "unknow",
+     name: resultCharacter.name ?? "unknown",
+     status: statusValues.getValue(resultCharacter.status ?? "unknown"),
+     species: resultCharacter.species ?? "unknown",
+     type: resultCharacter.type ?? "unknown",
+     gender: genderValues.getValue(resultCharacter.gender ?? "unknown"),
      origin: OriginCharacter(
-      name: resultCharacter.origin?.name ?? "unknow",
-      url: resultCharacter.origin?.url ?? "unknow"
+      name: resultCharacter.origin?.name ?? "unknown",
+      url: resultCharacter.origin?.url ?? "unknown"
       ),
      location: LocationCharacterEntity(      
-      name: resultCharacter.location?.name ?? "unknow",
-      url: resultCharacter.location?.url ?? "unknow"      
+      name: resultCharacter.location?.name ?? "unknown",
+      url: resultCharacter.location?.url ?? "unknown"      
      ),
-     image: resultCharacter.image ?? "unknow",
+     image: resultCharacter.image ?? "unknown",
      episode: resultCharacter.episode ?? [],
-     url: resultCharacter.url ?? "unknow",
-     created: resultCharacter.created ?? "unknow");
+     url: resultCharacter.url ?? "unknown",
+     created: resultCharacter.created ?? "unknown");
 
   }
 }
+
+final genderValues = EnumValues({
+  "Female": Gender.female,
+  "Male": Gender.male,
+  "Genderless": Gender.genderless,
+  "unknown": Gender.unknown,
+});
+
+final statusValues = EnumValues({
+  "Alive": Status.alive,
+  "Dead": Status.dead,
+  "unknown": Status.unknown,
+});
+
+class EnumValues<T> {
+  Map<String, T> map;
+  late Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+  
+  T getValue(String key) {
+    return map[key]!;
+  }
+
+  Map<T, String> get reverse {
+    reverseMap = map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
+  }
+}
+
