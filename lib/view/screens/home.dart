f466b913b468387e1762b0331d1b3fbd63761a57
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-31 23:30:45 -0500

    Implemented ImageError in Home

--- lib/view/screens/home.dart
+++ lib/view/screens/home.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/blocs/bloc_characters/characters_bloc.dart';
+import 'package:rick_and_morty/infrastructure/datasource_imp/datasource_imp_rick_and_morty.dart';
+import 'package:rick_and_morty/infrastructure/models/model_character.dart';
+import 'package:rick_and_morty/view/widgets/image_error.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final blocCharacter = context.watch<CharactersBloc>(); 
+    // final data =  DataSourceImpRickAndMorty().getCharacter(1);
     return Scaffold(        
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
-        bottomNavigationBar: Text('Hello World'),
-        body: Center(
+        body: blocCharacter.state.characterList != null ? Center(child:
+         ImageError()) :
+        //  Text("DAta")) :
+          Center(
           child: Column(
             children: [
-              ElevatedButton(onPressed: (){}, child: Text("button")),
-              Text('Hello World'),
+              ElevatedButton(onPressed: ()async{
+              //   final data = await DataSourceImpRickAndMorty().response("character", {"queryParameter": ""}) as ModelCharacter;
+              //   print(data.results![0].name);
+              }, child: Text("button")),
+              // Text(data.toString()),
             ],
           ),
         ),
