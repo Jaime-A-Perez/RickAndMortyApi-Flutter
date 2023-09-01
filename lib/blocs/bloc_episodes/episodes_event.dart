34134bdc7d253d038386fd479dba1f28cd65b44a
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-31 21:14:50 -0500

    Blocs creation

---
+++ lib/blocs/bloc_episodes/episodes_event.dart
@@ -0,0 +1,4 @@
+part of 'episodes_bloc.dart';
+
+@immutable
+sealed class EpisodesEvent {}
---
+++ lib/blocs/bloc_episodes/episodes_state.dart
@@ -0,0 +1,6 @@
+part of 'episodes_bloc.dart';
+
+@immutable
+sealed class EpisodesState {}
+
+final class EpisodesInitial extends EpisodesState {}
