978a85f3ad09dc583111b5082e225eadc4a76be6
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-31 23:39:47 -0500

    Adjust importation into abstract class DataSourceRickAndMorty

--- lib/domine/datasources/datasource_rick_and_morty.dart
+++ lib/domine/datasources/datasource_rick_and_morty.dart
@@ -1,6 +1,6 @@
 import 'package:rick_and_morty/domine/entities/character.dart';
 import 'package:rick_and_morty/domine/entities/epidode.dart';
-import 'package:rick_and_morty/infrastructure/models/model_character.dart';
+import 'package:rick_and_morty/domine/entities/location.dart';
 /// Represents a data source for retrieving character, episode, and location data.
 abstract class DataSourceRickAndMorty {
 
