d4df00765cf6e71e00febe142aba9c0c7ec9e719
Jaime A Perez R <catecppcemp@gmail.com>
2023-09-01 10:58:58 -0500

    EpisodesPage creation

---
+++ lib/view/screens/episodes_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/blocs/bloc_episodes/episodes_bloc.dart';
+import 'package:rick_and_morty/view/widgets/card_episodes.dart';
+
+class EpisodesPage extends StatelessWidget {
+  const EpisodesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final blocEpisodes = context.watch<EpisodesBloc>();
+
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Episodes",
+          style: textTheme.displaySmall,
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: blocEpisodes.state.episodesList!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return CardEpisode(
+                    episode: blocEpisodes.state.episodesList![index],
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>Center(),
+                          ));
+                    },
+                  );
+                }),
+          )
+        ],
+      ),
+    ));
+  }
+}
