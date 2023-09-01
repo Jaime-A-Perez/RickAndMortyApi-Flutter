8665f48fdd444a6092a3ec149da45b60ad14b8ca
Jaime A Perez R <catecppcemp@gmail.com>
2023-09-01 13:52:50 -0500

    Widget CustomBottomNavigationBar Implementation and conexions

--- lib/view/screens/home.dart
+++ lib/view/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/blocs/bloc_bottom_bar/bottom_bar_bloc.dart';
 import 'package:rick_and_morty/blocs/bloc_characters/characters_bloc.dart';
 import 'package:rick_and_morty/infrastructure/datasource_imp/datasource_imp_rick_and_morty.dart';
 import 'package:rick_and_morty/infrastructure/models/model_character.dart';
@@ -7,6 +8,7 @@ import 'package:rick_and_morty/view/screens/characters_page.dart';
 import 'package:rick_and_morty/view/screens/episodes_screen.dart';
 import 'package:rick_and_morty/view/screens/locations_page.dart';
 import 'package:rick_and_morty/view/widgets/image_error.dart';
+import 'package:rick_and_morty/view/widgets/tab_view.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -14,24 +16,28 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final blocCharacter = context.watch<CharactersBloc>(); 
+    final blocBottomBar = context.watch<BottomBarBloc>(); 
+    final updateBottomBar = context.read<BottomBarBloc>();
+    final namePage = ["Characters", "Locations", "Episodes"];
+    final iconPage = [ Icon(Icons.person), Icon(Icons.fmd_good_sharp), Icon(Icons.movie)];
+    final onTap = [(){updateBottomBar.add(ChangeIndex(0));},
+                    (){updateBottomBar.add(ChangeIndex(1));},
+                    (){updateBottomBar.add(ChangeIndex(2));}];
+    final page = [CharacterPage(), LocationsPage(), EpisodesPage()];
     // final data =  DataSourceImpRickAndMorty().getCharacter(1);
     return 
     Scaffold(        
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
-        body: EpisodesPage()
-        // body: LocationsPage()
-        // body: CharacterPage()
-        //  blocCharacter.state.characterList == null ? 
-        // Center(child: ImageError()) :
-        //   Center( child: Column(
-        //     children: [
-        //       ElevatedButton(onPressed: ()async{
-        //       }, child: Text("button")), 
-        //     ],
-        //   ),
-        // ),
+        body: blocCharacter.state.characterList != null ?
+         page[blocBottomBar.state.pageIndex]:
+         ImageError(),
+        bottomNavigationBar: 
+        CustomBottomNavigationBar(
+          titles: namePage,
+          icons: iconPage,         
+          ontaps: onTap),
       );
   }
 }
