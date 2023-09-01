6ec29c4d568c25fdc5f375fa7d75b2f67fd8e3dc
Jaime A Perez R <catecppcemp@gmail.com>
2023-09-01 10:03:56 -0500

    LocationsPage creation

---
+++ lib/view/screens/locations_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/blocs/bloc_location/location_bloc.dart';
+import 'package:rick_and_morty/view/widgets/card_location.dart';
+
+class LocationsPage extends StatelessWidget {
+  const LocationsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final blocLocation = context.watch<LocationBloc>(); 
+
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Location",
+          style: textTheme.displaySmall,
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: blocLocation.state.locationsList?.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return CardLocation(
+                    location: blocLocation.state.locationsList![index],
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  Center()));
+                    },
+                  );
+                }),
+          )
+        ],
+      ),
+    ));
+  }
+}
