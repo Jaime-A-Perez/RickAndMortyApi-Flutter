33ddd93eafaf623814d49500d1b53b7828df2e15
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-31 22:54:34 -0500

    Intial event into LocationBloc

--- lib/blocs/bloc_location/location_bloc.dart
+++ lib/blocs/bloc_location/location_bloc.dart
@@ -2,14 +2,16 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 import 'package:rick_and_morty/domine/entities/location.dart';
+import 'package:rick_and_morty/infrastructure/datasource_imp/datasource_imp_rick_and_morty.dart';
 
 part 'location_event.dart';
 part 'location_state.dart';
 
 class LocationBloc extends Bloc<LocationEvent, LocationsState> {
   LocationBloc() : super(LocationsState()) {
-    on<LocationEvent>((event, emit) {
-      // TODO: implement event handler
+    on<InitCharacterBloc>((event, emit) async{
+      final List<Location> locations = await DataSourceImpRickAndMorty().getLocation(1);
+      emit(state.copywith(locationsList: locations));
     });
   }
 }
