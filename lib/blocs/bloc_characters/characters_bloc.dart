48a5ec10b5e4dacaec6928ce1102d5d8479ef0aa
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-31 22:39:42 -0500

    Initial event into CharacterBloc

--- lib/blocs/bloc_characters/characters_bloc.dart
+++ lib/blocs/bloc_characters/characters_bloc.dart
@@ -2,14 +2,16 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 import 'package:rick_and_morty/domine/entities/character.dart';
+import 'package:rick_and_morty/infrastructure/datasource_imp/datasource_imp_rick_and_morty.dart';
 
 part 'characters_event.dart';
 part 'characters_state.dart';
 
 class CharactersBloc extends Bloc<CharactersEvent, CharactersState> {
   CharactersBloc() : super(CharactersState()) {
-    on<CharactersEvent>((event, emit) {
-      // TODO: implement event handler
+    on<InitCharacterBloc>((event, emit) async{
+      final List<Character> characters = await DataSourceImpRickAndMorty().getCharacter(1);
+      emit(state.copywith(characterList: characters));
     });
   }
 }
