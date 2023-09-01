89ba5aae79734751536cdc42f2286fcf53b258f4
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-31 21:43:09 -0500

    Class EpisodeState creation

--- lib/blocs/bloc_episodes/episodes_state.dart
+++ lib/blocs/bloc_episodes/episodes_state.dart
@@ -1,6 +1,23 @@
 part of 'episodes_bloc.dart';
 
-@immutable
-sealed class EpisodesState {}
 
-final class EpisodesInitial extends EpisodesState {}
+class EpisodesState {
+  final List<Episode>? characterList;
+  final Episode? searchResult;
+  final String? queryParameters;
+  final bool? isActiveSearch;
+
+  EpisodesState({this.characterList, this.searchResult, this.queryParameters, this.isActiveSearch});
+
+  EpisodesState copywith({
+    List<Episode>? characterList,
+    Episode? searchResult,
+    String? queryParameters,
+    bool? isActiveSearch,
+  }) => EpisodesState(
+    characterList: characterList ?? this.characterList,
+    searchResult: searchResult,
+    queryParameters: queryParameters ?? this.queryParameters,
+    isActiveSearch:  isActiveSearch ?? this.isActiveSearch) ;
+  
+}
