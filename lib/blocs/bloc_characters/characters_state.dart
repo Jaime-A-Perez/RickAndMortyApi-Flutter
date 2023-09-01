71dce852ed6b74bd39a8cb038903da70b41c5a54
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-31 21:40:28 -0500

    Class CharactersState creation

--- lib/blocs/bloc_characters/characters_state.dart
+++ lib/blocs/bloc_characters/characters_state.dart
@@ -1,6 +1,23 @@
 part of 'characters_bloc.dart';
 
-@immutable
-sealed class CharactersState {}
+class CharactersState {
+  final List<Character>? characterList;
+  final Character? searchResult;
+  final String? queryParameters;
+  final bool? isActiveSearch;
 
-final class CharactersInitial extends CharactersState {}
+  CharactersState({this.characterList, this.searchResult, this.queryParameters, this.isActiveSearch});
+
+  CharactersState copywith({
+    List<Character>? characterList,
+    Character? searchResult,
+    String? queryParameters,
+    bool? isActiveSearch,
+  }) => CharactersState(
+    characterList: characterList ?? this.characterList,
+    searchResult: searchResult,
+    queryParameters: queryParameters ?? this.queryParameters,
+    isActiveSearch:  isActiveSearch ?? this.isActiveSearch) ;
+  
+}
+
